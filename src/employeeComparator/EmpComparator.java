82dcd40ab2fd74d0bf0b6d6e2f0e362e590275a5
Mark-Fitzgerald2 <35224637+Mark-Fitzgerald2@users.noreply.github.com>
2018-01-08 13:24:35 +0000

    Add files via upload

--- src/employeeComparator/EmpComparator.java
+++ src/employeeComparator/EmpComparator.java
@@ -2,8 +2,28 @@ package employeeComparator;
 
 import java.util.Comparator;
 public class EmpComparator implements Comparator<Employee> {
-	@Override
+	/*@Override
 	public int compare(Employee e1, Employee e2) {
 		return (int)(e1.getSalary()-e2.getSalary());
+	}*/
+	
+	/*@Override
+	public int compare(Employee e1, Employee e2) {
+		return (int)(e1.getID()-e2.getID());
+	}*/
+	
+	@Override
+	public int compare(Employee e1, Employee e2) {
+		if (e1.getName() == null) {
+	        return -1;
+	    }
+	    if (e2.getName() == null) {
+	        return 1;
+	    }
+	    if (e1.getName().equals( e2.getName() )) {
+	        return 0;
+	    }
+	    return e1.getName().compareTo(e2.getName());
 	}
+	
 }
